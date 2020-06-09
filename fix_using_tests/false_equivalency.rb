80c7572dbad4944fcb3c1e1cf892e6a3146c12e2
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-09 22:30:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,10 +13,10 @@ end
 require 'pry'
 def selection(num)
   if num = 1
-    binding.pry
+   
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-  binding.pry
+ 
      "HAM HAM HAM IN MY TUMMY"
   end
 end
