63c55bbbb5be95a0896b0776e161165b961d4818
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-09 23:08:50 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,7 @@ require 'pry'
 def snake_it_up(string)
   if string[0] = "sssssssssssurprise!"
     
-    10 * "sssssssssssurprise!" + string
+     "sssssssssssurprise!" + string
   else
     string
   end
